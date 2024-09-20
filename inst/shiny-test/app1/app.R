e24fd016c6258818d124a15a99d41eb2fab6b22f
Obi Obianom <oobianom@umaryland.edu>
2024-09-20 13:17:14 -0400

    Update app.R

--- inst/shiny-test/app1/app.R
+++ inst/shiny-test/app1/app.R
@@ -48,7 +48,23 @@ ui <- fluidPage(
         actionButton("test3", "Click to save")
       )
     )
-  )
+  ),
+
+  textInput("test2", "Input change"),
+  actionButton("test3", "Click to save"),
+  textInput("test2", "Input change"),
+  actionButton("test3", "Click to save"),
+  HTML("Lorem ipsum odor amet, consectetuer adipiscing elit. In donec vel morbi sem litora mus. Ad interdum semper congue ultricies maecenas sodales enim ut. Leo facilisis varius egestas bibendum; quis litora purus. Varius penatibus nulla volutpat tortor tristique vivamus nostra nisi suspendisse. Eros per taciti magna consequat phasellus; eget varius. Maximus sociosqu nisl sem risus finibus vivamus? Mi id lectus; rutrum amet montes interdum.
+
+Nibh etiam urna est; potenti a montes imperdiet volutpat vivamus. Augue litora venenatis eleifend maecenas, platea purus. Neque iaculis efficitur est erat nascetur urna consectetur laoreet. Consectetur leo vehicula velit, parturient metus commodo. Hendrerit suspendisse hac magnis fusce facilisi fusce duis. Ullamcorper euismod ullamcorper maecenas; himenaeos arcu arcu lectus. Dui donec eros etiam lacus bibendum nulla.
+<p>
+Magnis duis congue commodo sodales dictum amet quam. Elementum rhoncus feugiat netus nisl himenaeos hac. Sodales vivamus ullamcorper mauris at torquent aliquam. Nisl egestas dapibus venenatis; imperdiet imperdiet lacinia malesuada accumsan. Scelerisque habitasse efficitur cubilia purus primis euismod non sed. Luctus nisi neque fringilla rhoncus eros. Viverra volutpat senectus curae accumsan finibus. Imperdiet fringilla ridiculus nunc porta amet ut parturient conubia.
+</p>
+Rutrum tincidunt gravida facilisi; ornare cras nascetur parturient. Auctor accumsan libero nullam facilisi conubia lacinia. Interdum senectus platea dui parturient in. Vivamus elit nunc sociosqu ornare curae enim. Facilisis senectus commodo imperdiet quis platea dolor senectus. Et non felis venenatis nulla neque luctus. Velit ante auctor nostra venenatis, vehicula ipsum volutpat risus. Massa sem mollis metus montes adipiscing eros odio viverra suspendisse. Velit dignissim cubilia neque vehicula lacus nibh sagittis aptent.
+
+Libero fringilla himenaeos quis etiam convallis penatibus pharetra vitae. Mattis habitasse felis feugiat amet eget adipiscing massa pharetra. Ac malesuada consectetur quisque cras penatibus per magnis. Elementum nullam imperdiet nam sagittis rhoncus quisque. Efficitur pulvinar lectus egestas pellentesque eleifend nam inceptos luctus. Consequat nec ad est; vitae felis euismod magnis. Cras posuere nibh sit enim lorem pretium class eros morbi. Maecenas per donec tortor lorem nec vel. Taciti efficitur tempor ligula; nascetur quis aptent."),
+
+  footerPanel("Hellow", rightContent = "Find me", bg.col = "brown")
 )
 
 # Run the application
