85aab140eedd8aab051a6d108e03a5699da74b11
Obi Obianom <oobianom@umaryland.edu>
2025-01-15 02:09:23 -0500

    Update card.pro-package.R

--- R/card.pro-package.R
+++ R/card.pro-package.R
@@ -7,6 +7,9 @@
 "_PACKAGE"
 
 .onLoad <- function(libname,pkgname){
-
+  utils::globalVariables(c("accordioniId"))
 }
 
+
+
+color.choice <- c( "redLight", "red","white", "green", "greenDark", "greenLight", "purple", "magenta", "pink", "pinkDark", "blueLight", "teal", "blue", "blueDark", "darken", "yellow", "orange", "orangeDark")
